f30f1439f8c0aa6bcceaf809dff0f7abcc4e2673
chetansomkuwar254 <146824400+chetansomkuwar254@users.noreply.github.com>
2024-08-16 17:55:56 +0000

    --- Auto Git Commit ---

---
+++ infra_scm/variable.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type = string
+  description = "creating the repo using variable-tf"
+}
+
+variable "visibility" {
+  type = string
+  description = "giving a visibility variable-tf"
+}
